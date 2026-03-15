e6b5828bd79f2574fab04837f8da33a80d22c6e8
Hung hay ho <hungok2909@gmail.com>
2026-03-15 16:17:19 +0700

    enhance Socket.IO configuration and controller for JWT authentication and user management

--- src/main/java/com/teamtobo/tobochatserver/controllers/SocketIOController.java
+++ src/main/java/com/teamtobo/tobochatserver/controllers/SocketIOController.java
@@ -4,34 +4,42 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class SocketIOController {
     private final SocketIOServer server;
+    private final JwtDecoder jwtDecoder;
 
-    public SocketIOController(SocketIOServer server) {
+    public SocketIOController(SocketIOServer server, JwtDecoder jwtDecoder) {
         this.server = server;
+        this.jwtDecoder = jwtDecoder;
         server.addConnectListener(onConnected());
         server.addDisconnectListener(onDisconnected());
     }
 
     private ConnectListener onConnected() {
         return client -> {
-            String userId = client.getHandshakeData().getSingleUrlParam("userId");
-            if (userId != null && !userId.isEmpty()) {
+            String token = client.getHandshakeData().getSingleUrlParam("token");
+            if (token != null) {
+                Jwt jwt = jwtDecoder.decode(token);
+                String userId = jwt.getSubject();
+
                 client.joinRoom(userId);
+                client.set("userId", userId);
+
                 log.info("User [{}] đã online", userId);
-            } else {
-                client.disconnect();
             }
         };
     }
 
     private DisconnectListener onDisconnected() {
         return client -> {
-            String userId = client.getHandshakeData().getSingleUrlParam("userId");
+            String userId = client.get("userId");
             if (userId != null) {
                 log.info("User [{}] đã offline", userId);
             }
