f8196c43f636866a3c118c2ce30a746c3181dc86
Hung hay ho <hungok2909@gmail.com>
2026-04-12 21:24:39 +0700

    added InboxStatus enum

--- src/main/java/com/teamtobo/tobochatserver/entities/enums/InboxStatus.java
+++ src/main/java/com/teamtobo/tobochatserver/entities/enums/InboxStatus.java
@@ -1,2 +1,6 @@
-package com.teamtobo.tobochatserver.entities.enums;public enum InboxStatus {
+package com.teamtobo.tobochatserver.entities.enums;
+
+public enum InboxStatus {
+    ACTIVE, // người chủ động nhắn hoặc đã là bạn bè
+    PENDING // người nhận tin nhắn từ người lạ
 }
