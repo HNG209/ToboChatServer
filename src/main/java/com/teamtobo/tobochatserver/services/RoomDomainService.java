fe2d7b4087710d8a225222bd7f9a8cec59802f03
Hung hay ho <hungok2909@gmail.com>
2026-04-17 22:36:57 +0700

    refractored services, tidy up code

--- src/main/java/com/teamtobo/tobochatserver/services/RoomDomainService.java
+++ src/main/java/com/teamtobo/tobochatserver/services/RoomDomainService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface RoomDomainService {
     void createRoom(String userId, RoomCreateRequest request, RoomType roomType);
     void addMemberToGroup(String roomId, String inviterId, List<String> targetUserIds);
-
+    String getOrCreateDMRoom(String userId, String otherId);
     void approveMember(String roomId, String adminId, String targetUserId, boolean accept);
     void toggleApproveMember(String roomId, String userId);
 }
