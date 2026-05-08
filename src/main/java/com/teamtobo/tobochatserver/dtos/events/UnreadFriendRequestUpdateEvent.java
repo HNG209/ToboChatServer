5a85159096f7d1ad83bf9cc5024caed853a8e36b
Bao To <you@example.com>
2026-05-08 11:44:59 +0700

    feat: implement update and reset friend requests function

---
+++ src/main/java/com/teamtobo/tobochatserver/dtos/events/UnreadFriendRequestUpdateEvent.java
@@ -0,0 +1,12 @@
+package com.teamtobo.tobochatserver.dtos.events;
+
+import com.teamtobo.tobochatserver.entities.enums.UnreadUpdateType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UnreadFriendRequestUpdateEvent {
+    private String userId;
+    UnreadUpdateType type;
+}
